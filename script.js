539170b09be762e2a265ef679f69ae1ec98583a0
Marius Gynnild <mwgynnild@gmail.com>
2019-01-27 01:33:41 +0100

    Create a homepage that opens on new tabs

---
+++ script.js
@@ -0,0 +1,43 @@
+const digitsHours   = document.querySelector('.hours');
+const digitsMinutes = document.querySelector('.minutes');
+const digitsSeconds = document.querySelector('.seconds');
+const digitalClock  = document.querySelector('.clock');
+const dateElement   = document.querySelector('.date');
+const dayElement    = document.querySelector('.day');
+const header        = document.querySelector('.header');
+const days          = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+function setTime() {
+    const now     = new Date();
+    const date    = now.toLocaleDateString();
+    const day     = days[now.getDay()];
+    const hours   = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    dateElement.innerHTML = date;
+    dayElement.innerHTML  = day;
+    populateDigitalClock(hours, minutes, seconds);
+    digitalClock.classList.remove('hidden');
+}
+
+function populateDigitalClock(h, m, s) {
+    digitsHours.innerHTML   = formatDigits(h);
+    digitsMinutes.innerHTML = formatDigits(m);
+    digitsSeconds.innerHTML = formatDigits(s);
+}
+
+function formatDigits(n) {
+    return n > 9 ? n : "0" + n;
+}
+
+function toggleSeconds() {
+    digitsSeconds.classList.toggle('hidden');
+}
+
+function toggleDarkMode() {
+    header.classList.toggle('dark-mode');
+}
+
+setInterval(setTime, 1000); // 1 second
+digitalClock.addEventListener('click', toggleSeconds);
+header.addEventListener('dblclick', toggleDarkMode);
